3bb7c80e887b77cdcbcc027178ee1b36e3173db9
Angellucy460 <angelliona38@gmail.com>
2025-09-16 09:10:27 +0300

    new

--- app/routes/student.py
+++ app/routes/student.py
@@ -2,18 +2,23 @@ from flask import Blueprint, jsonify, request
 
 
 #create student blueprint
-student_bp=Blueprint("student",__name__)
+student_bp = Blueprint("student", __name__, url_prefix="/student")
 
 
     
 
 #routes and controller logic
-@student_bp.route("/student/add", methods=["POST"])
+@student_bp.route("/add", methods=["POST"])
 def add_user():
     print("add user was hit")
     return "adding a user"
 
-@student_bp.route("/student/list", methods=["GET"])
-def list_user():
-    print("Lists Students")
+@student_bp.route("/edit", methods=["PUT"])
+def edit_student():
+    print("add user was hit")
+    return "Edit a student"
+
+@student_bp.route("/list", methods=["GET"])
+def list_users():
+    print("List Students")
     return "List all students"
